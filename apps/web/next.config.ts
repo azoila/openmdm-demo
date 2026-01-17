96c4a89c5bf656f16d4e5862552a764805d503aa
andersonk <andersondevmail@gmail.com>
2026-01-17 12:08:29 -0400

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@openmdm-demo/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
